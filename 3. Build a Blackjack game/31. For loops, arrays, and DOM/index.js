ac13f7f02add0d27df133f9a61ac6ce1a02e2b4f
Akechi Goro <zane41181@gmail.com>
2025-02-07 17:16:41 +0300

    ex 3.31

--- 3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
+++ 3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
@@ -1,5 +1,13 @@
-let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
-let greetingEl = document.getElementById("greeting-el")
+let sentence = ["Hello ", "my ", "name ", "is ", "Per"];
+let greetingEl = document.getElementById("greeting-el");
 
 // Render the sentence in the greetingEl paragraph using a for loop and .textContent
 
+
+function m(){
+    for (let i = 0; i < 5; i += 1) {
+        greetingEl.textContent = sentence[i];
+      }
+    
+
+}
