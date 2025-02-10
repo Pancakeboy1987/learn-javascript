36566a0aa1c2c671ff390ef48fbeeb31986a8b81
Akechi Goro <zane41181@gmail.com>
2025-02-10 18:34:11 +0300

    ex 5.6-5.7

--- 5. Build a Chrome Extension/07. Your turn to refactor/index.js
+++ 5. Build a Chrome Extension/07. Your turn to refactor/index.js
@@ -2,8 +2,11 @@
 // when you click the SAVE INPUT button
 
 function saveLead() {
-    console.log("Button clicked!")
+  console.log("Button clicked!");
 }
 
+let inputButton = document.getElementById("input-btn");
 
-
+inputButton = addEventListener("click", function () {
+  console.log("saved!");
+});
